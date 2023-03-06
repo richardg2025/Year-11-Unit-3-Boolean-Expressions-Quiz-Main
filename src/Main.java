c7e58991aa9f1c314033e1a699d225233f3e5c2d
Adam <adam.ewing.nz@gmail.com>
2023-03-06 11:10:31 +0800

    Added comments in Main class.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    // Question 1 - calculateGrade
+
+    // Question 2 - fizzBuzz
+
+    // Question 3 - frontBack
+
+    // Question 4 - twoAsOne
+
+    // Question 5 - endUp
 }
